9089c80d0e0806af293ebcf72d14bcb24cc29ae5
diegomedinapadilla <diemp94@gmail.com>
2024-03-05 12:56:10 -0700

    Added moviesDpApi, retrofitClient and models

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,11 @@ dependencies {
     //Coil
     implementation("io.coil-kt:coil-compose:2.5.0")
 
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
