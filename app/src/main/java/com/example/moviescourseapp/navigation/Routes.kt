6fa630abeda763713ee93889d12eb3857a282d55
Adolfo Chavez <adolfo.chavez1295@gmail.com>
2024-03-26 15:46:51 -0600

    Added bottom navigation

---
+++ app/src/main/java/com/example/moviescourseapp/navigation/Routes.kt
@@ -0,0 +1,10 @@
+package com.example.moviescourseapp.navigation
+
+object Routes {
+
+  val HomeScreen =  "home"
+  val FavoritesScreen = "favs"
+  val NowPlayingScreen = "nowplaying"
+  val DetailsScreen = "details"
+
+}
