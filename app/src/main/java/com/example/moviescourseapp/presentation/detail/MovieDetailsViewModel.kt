2f552684632f2f518ec1548b9900166595da1f55
Adolfo Chavez <adolfo.chavez1295@gmail.com>
2024-05-29 16:59:08 -0600

    Fixed view model api call with launchedeffect

--- app/src/main/java/com/example/moviescourseapp/presentation/detail/MovieDetailsViewModel.kt
+++ app/src/main/java/com/example/moviescourseapp/presentation/detail/MovieDetailsViewModel.kt
@@ -4,22 +4,53 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.moviescourseapp.data.MoviesRepository
+import com.example.moviescourseapp.presentation.home.ErrorMessage
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.net.ConnectException
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.seconds
 
 @HiltViewModel
 class MovieDetailsViewModel @Inject constructor(
   private val movieRepository: MoviesRepository
 ) : ViewModel() {
 
+  private val _movieDetailsUiState = MutableStateFlow(MovieDetailsUiState())
+  val movieDetailsUiState = _movieDetailsUiState.asStateFlow()
+
   fun getMovieDetails(movieId: String) {
+
+    Log.d("adolfo", "getMovieDetails: paso por aqui")
+
     viewModelScope.launch {
+
+      _movieDetailsUiState.update {
+        it.copy(isLoading = true, error = null)
+      }
+
+      delay(1.seconds)
+
       try {
         val movieDetail = movieRepository.getMovieDetails(movieId = movieId)
-        Log.d("adolfo", "getMovieDetails: $movieDetail")
+        _movieDetailsUiState.update {
+          it.copy(isLoading = false, movieDetailsModel = movieDetail)
+        }
       } catch (e: Exception) {
-        Log.e("adolfo", "error: $e")
+        val error = when (e) {
+          is ConnectException -> ErrorMessage.INTERNET_CONNECTION
+          else -> ErrorMessage.DEFAULT
+        }
+
+        _movieDetailsUiState.update {
+          it.copy(isLoading = false, error = error)
+        }
+
       }
     }
   }
