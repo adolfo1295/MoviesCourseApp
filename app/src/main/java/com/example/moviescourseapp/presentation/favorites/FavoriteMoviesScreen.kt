6fe34072cc32c1a8487168823f2847c59eab6b08
Adolfo Chavez <adolfo.chavez1295@gmail.com>
2025-01-16 12:06:44 -0600

    Fix for swipe to dismiss

--- app/src/main/java/com/example/moviescourseapp/presentation/favorites/FavoriteMoviesScreen.kt
+++ app/src/main/java/com/example/moviescourseapp/presentation/favorites/FavoriteMoviesScreen.kt
@@ -11,12 +11,12 @@ import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SwipeToDismiss
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDismissState
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -32,77 +32,78 @@ import com.example.moviescourseapp.presentation.home.MovieCard
 
 @Composable
 fun FavoriteMoviesScreen(
-    viewModel: FavoriteMoviesViewModel = hiltViewModel(),
-    navigateToDetails: (MovieModel) -> Unit
+  viewModel: FavoriteMoviesViewModel = hiltViewModel(),
+  navigateToDetails: (MovieModel) -> Unit
 ) {
 
-    val favoriteMoviesUiState by viewModel.favoriteMoviesUiState.collectAsStateWithLifecycle()
-    val context = LocalContext.current
+  val favoriteMoviesUiState by viewModel.favoriteMoviesUiState.collectAsStateWithLifecycle()
+  val context = LocalContext.current
 
 
-    LaunchedEffect(key1 = favoriteMoviesUiState.errorMessage) {
-        favoriteMoviesUiState.errorMessage?.let { error ->
-            Toast.makeText(context,error.message, Toast.LENGTH_SHORT).show()
-        }
+  LaunchedEffect(key1 = favoriteMoviesUiState.errorMessage) {
+    favoriteMoviesUiState.errorMessage?.let { error ->
+      Toast.makeText(context, error.message, Toast.LENGTH_SHORT).show()
     }
+  }
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        if (favoriteMoviesUiState.isLoading) {
-            //mostrar loading
-            CircularProgressIndicator(
-                modifier = Modifier.align(Alignment.Center)
+  Box(modifier = Modifier.fillMaxSize()) {
+    if (favoriteMoviesUiState.isLoading) {
+      //mostrar loading
+      CircularProgressIndicator(
+        modifier = Modifier.align(Alignment.Center)
+      )
+    } else {
+      val movieList = favoriteMoviesUiState.movieList
+      if (movieList.isNotEmpty()) {
+        //mostrar peliculas
+        LazyVerticalStaggeredGrid(
+          verticalItemSpacing = 8.dp,
+          horizontalArrangement = Arrangement.spacedBy(4.dp),
+          contentPadding = PaddingValues(horizontal = 8.dp, vertical = 4.dp),
+          columns = StaggeredGridCells.Fixed(2),
+          content = {
+            items(movieList, key = { movieModel ->
+              movieModel.id
+            }) { movieModel ->
+
+              val dismissState = rememberSwipeToDismissBoxState(
+              confirmValueChange = { dismissValue ->
+                if (dismissValue == SwipeToDismissBoxValue.EndToStart) {
+                  viewModel.deleteMovieFromFavorites(movieModel)
+                  Toast.makeText(context, "Pelicula eliminada correctamente", Toast.LENGTH_SHORT)
+                    .show()
+                  true
+                } else {
+                  false
+                }
+              }
             )
-        } else {
-            val movieList = favoriteMoviesUiState.movieList
-            if (movieList.isNotEmpty()) {
-                //mostrar peliculas
-                LazyVerticalStaggeredGrid(
-                    verticalItemSpacing = 8.dp,
-                    horizontalArrangement = Arrangement.spacedBy(4.dp),
-                    contentPadding = PaddingValues(horizontal = 8.dp, vertical = 4.dp),
-                    columns = StaggeredGridCells.Fixed(2),
-                    content = {
-                        items(movieList, key = { movieModel ->
-                            movieModel.id
-                        }) { movieModel ->
 
-                            val dismissState = rememberDismissState(
-                                confirmValueChange = { dismissValue ->
-                                    if (dismissValue == DismissValue.DismissedToStart) {
-                                        viewModel.deleteMovieFromFavorites(movieModel)
-                                        Toast.makeText(context,"Pelicula eliminada correctamente",Toast.LENGTH_SHORT).show()
-                                        true
-                                    } else {
-                                        false
-                                    }
-                                }
-                            )
-
-                            SwipeToDismiss(
-                                state = dismissState,
-                                background = {},
-                                dismissContent = {
-                                    MovieCard(
-                                        movieModel = movieModel,
-                                        onMovieClick = { movieClicked ->
-                                            navigateToDetails(movieClicked)
-                                        }
-                                    )
-                                }
-                            )
-                        }
+              SwipeToDismissBox(
+                state = dismissState,
+                backgroundContent = {},
+                content = {
+                  MovieCard(
+                    movieModel = movieModel,
+                    onMovieClick = { movieClicked ->
+                      navigateToDetails(movieClicked)
                     }
-                )
-            } else {
-                Text(
-                    modifier = Modifier.align(Alignment.Center),
-                    style = MaterialTheme.typography.titleLarge,
-                    textAlign = TextAlign.Center,
-                    color = MaterialTheme.colorScheme.onSurface,
-                    text = "No tienes ninguna pelicula en favoritos, agrega una!"
-                )
+                  )
+                }
+              )
             }
-        }
+          }
+        )
+      } else {
+        Text(
+          modifier = Modifier.align(Alignment.Center),
+          style = MaterialTheme.typography.titleLarge,
+          textAlign = TextAlign.Center,
+          color = MaterialTheme.colorScheme.onSurface,
+          text = "No tienes ninguna pelicula en favoritos, agrega una!"
+        )
+      }
     }
+  }
 
 }
